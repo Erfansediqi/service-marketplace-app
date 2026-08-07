7d5478638326fe365036c94ccf8a96dcb4f047ec
Khalid <khalid@Khalids-MacBook-Pro.local>
2026-08-07 16:19:50 +0530

    Remove leftover merge conflict markers

--- scripts/reset-project.js
+++ scripts/reset-project.js
@@ -2,11 +2,7 @@
 
 /**
  * This script is used to reset the project to a blank state.
-<<<<<<< HEAD
  * It deletes or moves the /src and /scripts directories to /example based on user input and creates a new /src/app directory with an index.tsx and _layout.tsx file.
-=======
- * It deletes or moves the /app, /components, /hooks, /scripts, and /constants directories to /app-example based on user input and creates a new /app directory with an index.tsx and _layout.tsx file.
->>>>>>> a7d3e1ab3cecbba72bd3293840d4cb14cd4f89b7
  * You can remove the `reset-project` script from package.json and safely delete this file after running it.
  */
 
@@ -15,7 +11,6 @@ const path = require("path");
 const readline = require("readline");
 
 const root = process.cwd();
-<<<<<<< HEAD
 const oldDirs = ["src", "scripts"];
 const exampleDir = "example";
 const newAppDir = "src/app";
@@ -38,28 +33,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
-=======
-const oldDirs = ["app", "components", "hooks", "constants", "scripts"];
-const exampleDir = "app-example";
-const newAppDir = "app";
-const exampleDirPath = path.join(root, exampleDir);
-
-const indexContent = `import { Text, View } from "react-native";
-
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
->>>>>>> a7d3e1ab3cecbba72bd3293840d4cb14cd4f89b7
 `;
 
 const layoutContent = `import { Stack } from "expo-router";
@@ -99,31 +72,19 @@ const moveDirectories = async (userInput) => {
       }
     }
 
-<<<<<<< HEAD
     // Create new /src/app directory
     const newAppDirPath = path.join(root, newAppDir);
     await fs.promises.mkdir(newAppDirPath, { recursive: true });
     console.log("\n📁 New /src/app directory created.");
-=======
-    // Create new /app directory
-    const newAppDirPath = path.join(root, newAppDir);
-    await fs.promises.mkdir(newAppDirPath, { recursive: true });
-    console.log("\n📁 New /app directory created.");
->>>>>>> a7d3e1ab3cecbba72bd3293840d4cb14cd4f89b7
 
     // Create index.tsx
     const indexPath = path.join(newAppDirPath, "index.tsx");
     await fs.promises.writeFile(indexPath, indexContent);
-<<<<<<< HEAD
     console.log("📄 src/app/index.tsx created.");
-=======
-    console.log("📄 app/index.tsx created.");
->>>>>>> a7d3e1ab3cecbba72bd3293840d4cb14cd4f89b7
 
     // Create _layout.tsx
     const layoutPath = path.join(newAppDirPath, "_layout.tsx");
     await fs.promises.writeFile(layoutPath, layoutContent);
-<<<<<<< HEAD
     console.log("📄 src/app/_layout.tsx created.");
 
     console.log("\n✅ Project reset complete. Next steps:");
@@ -131,15 +92,6 @@ const moveDirectories = async (userInput) => {
       `1. Run \`npx expo start\` to start a development server.\n2. Edit src/app/index.tsx to edit the main screen.\n3. Put all your application code in /src, only screens and layout files should be in /src/app.${
         userInput === "y"
           ? `\n4. Delete the /${exampleDir} directory when you're done referencing it.`
-=======
-    console.log("📄 app/_layout.tsx created.");
-
-    console.log("\n✅ Project reset complete. Next steps:");
-    console.log(
-      `1. Run \`npx expo start\` to start a development server.\n2. Edit app/index.tsx to edit the main screen.${
-        userInput === "y"
-          ? `\n3. Delete the /${exampleDir} directory when you're done referencing it.`
->>>>>>> a7d3e1ab3cecbba72bd3293840d4cb14cd4f89b7
           : ""
       }`
     );
@@ -149,11 +101,7 @@ const moveDirectories = async (userInput) => {
 };
 
 rl.question(
-<<<<<<< HEAD
   "Do you want to move existing files to /example instead of deleting them? (Y/n): ",
-=======
-  "Do you want to move existing files to /app-example instead of deleting them? (Y/n): ",
->>>>>>> a7d3e1ab3cecbba72bd3293840d4cb14cd4f89b7
   (answer) => {
     const userInput = answer.trim().toLowerCase() || "y";
     if (userInput === "y" || userInput === "n") {
